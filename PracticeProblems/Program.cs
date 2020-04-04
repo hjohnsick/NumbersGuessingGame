8232ae22bf9d7e6c25fd3e379b29fec38de56daa
heather <hjohnsick@yahoo.com>
2020-04-04 11:46:26 -0400

    Program to find the deepest lake

--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PracticeProblems
 {
@@ -6,32 +8,32 @@ namespace PracticeProblems
     {
         static void Main(string[] args)
         {
-            int attempts, secret_number, guess;
-            Random Rnd = new Random(Guid.NewGuid().GetHashCode());
+            const int LAKES = 10;
+            int i;
+            double maximum;
+            double[] depths = new double[LAKES];
+            string[] name = new string[LAKES];
 
-            secret_number = Rnd.Next(1, 101);
-
-            attempts = 1;
-
-            Console.WriteLine("Enter a guess: ");
-            guess = int.Parse(Console.ReadLine());
-            while (guess != secret_number)
+            for (i = 0; i <= LAKES - 1; i++)
             {
-                if(guess > secret_number)
-                {
-                    Console.WriteLine("Your guess is bigger than my secret number.");
-                }
-                else
-                {
-                    Console.WriteLine("Your guess is smaller than my secret number.");
-                }
-                attempts += 1;
-
-                Console.WriteLine("Try again!  Enter a guess: ");
-                guess = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the name of Lake No " + (i + 1));
+                name[i] = Console.ReadLine();
+                Console.WriteLine("Enter depth for Lake " + name[i]);
+                depths[i] = Double.Parse(Console.ReadLine());
             }
-            Console.WriteLine("You found it!");
-            Console.WriteLine("Attempts: " + attempts);
-        }
+
+            maximum = depths[0];
+            string m_name = name[0];
+            for (i = 1; i <= LAKES - 1; i++)
+            {
+                if (depths[i] > maximum)
+                {
+                    maximum = depths[i];
+                    m_name = name[i];
+                }
+            }
+            Console.WriteLine("The deepest lake is " + m_name);
+            Console.ReadKey();
+        }     
     }
 }
